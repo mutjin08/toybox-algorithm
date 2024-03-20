34b697d4bdb5cc5dbce5ceaf72a062697d09801b
mutjin08 <mutjin08@gmail.com>
2024-03-20 10:35:34 +0900

    24-03-20

--- Samsung/14501_퇴사.py
+++ Samsung/14501_퇴사.py
@@ -1,4 +1,19 @@
+
 n = int(input())
-menu = []
-for _ in range(menu):
-    menu.append(list(map(int, input().split())))
+schedule = [[0, 0]] #기간, 금액
+for _ in range(n):
+    schedule.append(list(map(int, input().split())))
+
+dp = [0]*(n+2)
+
+#n일~1일
+for i in range(n+1, 0, -1):
+    time, pay = schedule[i]
+
+    if i-time <= 0:
+        dp[i] = dp[i-1]
+    else:
+        dp[i] = max(dp[i-1], dp[i-time]+pay)
+
+
+print(dp)
